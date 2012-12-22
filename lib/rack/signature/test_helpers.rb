34f93f2ee5936790470c5e2415863196f888fff2
Robert Evans <robert@codewranglers.org>
2012-12-21 22:19:38 -0800

    Updated the test helpers

--- lib/rack/signature/test_helpers.rb
+++ lib/rack/signature/test_helpers.rb
@@ -1,33 +1,10 @@
 require_relative 'build_message'
 require_relative 'hmac_signature'
+require_relative 'sort_query_params'
 
 module Rack
   module Signature
     module TestHelpers
-      include Rack::Test::Methods
-
-      def generate_shared_token; ::SecureRandom.hex(8); end
-
-      def convert_hash_to_string(params)
-        params.map {|p| p.join('=')}.join('&')
-      end
-
-      def stringify_request_message(env)
-        ::Rack::Signature::BuildMessage.new(env).build!
-      end
-
-      def hmac_message(key, message)
-        ::Rack::Signature::HmacSignature.new(key, message).sign
-      end
-
-      def setup_request(uri, opts, key)
-        env  = ::Rack::MockRequest.env_for(uri, opts)
-        msg  = stringify_request_message(env)
-        sig  = hmac_message(key, msg)
-
-
-        { signature: sig, message: msg, env: env, key: key }
-      end
 
     end
   end
