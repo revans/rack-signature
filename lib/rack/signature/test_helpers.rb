c02f5665f654f4fc4d159e3dc84ac60c73dabf55
Robert Evans <robert@codewranglers.org>
2012-12-21 22:36:49 -0800

    test helpers

--- lib/rack/signature/test_helpers.rb
+++ lib/rack/signature/test_helpers.rb
@@ -1,10 +1,31 @@
 require_relative 'build_message'
 require_relative 'hmac_signature'
 require_relative 'sort_query_params'
+require 'rack'
 
 module Rack
   module Signature
     module TestHelpers
+
+      def convert_hash_to_string(params)
+        params.map { |p| p.join('=')}.join('&')
+      end
+
+      def stringify_request_message(env)
+        ::Rack::Signature::BuildMessage.new(env).build!
+      end
+
+      def hmac_message(key, message)
+        ::Rack::Signature::HmacSignature.new(key, message).sign
+      end
+
+      def setup_request(uri, opts, key)
+        env = ::Rack::MockRequest.env_for(uri, opts)
+        msg = stringify_request_message(env)
+        sig = hmac_message(key, msg)
+
+        { signature: sig, message: msg, env: env, key: key }
+      end
 
     end
   end
